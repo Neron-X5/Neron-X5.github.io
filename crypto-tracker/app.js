108f6de09def67de5c523677545977d9a1610e50
Divy Gupta <divya.gupta@ticketmaster.com>
2019-07-13 02:36:29 +0530

    Crypto tracker URL changed

--- crypto-tracker/app.js
+++ crypto-tracker/app.js
@@ -8,7 +8,7 @@ const DEV_KEY = 'f1fdbebf-0d33-4134-8a1d-7963b02958ad';
 const IS_DEV = window.location.hostname === '0.0.0.0';
 
 const fetchCoinData = () => {
-    const request = new Request(SANDBOX_URL, {
+    const request = new Request(PRO_URL, {
         method: 'GET',
         // mode: 'no-cors',
         body: null,
